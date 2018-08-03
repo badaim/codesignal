49daf3b377180bae2cacfb9a0ff864ff61a7eed4
Krzysztof Srokowski <ksrokowski@gmail.com>
2018-08-03 19:00:43 +0200

    Create centuryFromYear.py

---
+++ centuryFromYear.py
@@ -0,0 +1,43 @@
+def centuryFromYear(year):
+    if year >= 1 and year <= 100 :
+        return 1
+    elif year > 100 and year <= 200 :
+        return 2
+    elif year > 200 and year <= 300 :
+        return 3
+    elif year > 300 and year <= 400 :
+        return 4
+    elif year > 400 and year <= 500 :
+        return 5
+    elif year > 500 and year <= 600 :
+        return 6
+    elif year > 600 and year <= 700 :
+        return 7
+    elif year > 700 and year <= 800 :
+        return 8
+    elif year > 800 and year <= 900 :
+        return 9
+    elif year > 900 and year <= 1000 :
+        return 10
+    elif year > 1000 and year <= 1100 :
+        return 11
+    elif year > 1100 and year <= 1200 :
+        return 12
+    elif year > 1200 and year <= 1300 :
+        return 13
+    elif year > 1300 and year <= 1400 :
+        return 14
+    elif year > 1400 and year <= 1500 :
+        return 15
+    elif year > 1500 and year <= 1600 :
+        return 16
+    elif year > 1600 and year <= 1700 :
+        return 17
+    elif year > 1700 and year <= 1800 :
+        return 18
+    elif year > 1800 and year <= 1900 :
+        return 19
+    elif year > 1900 and year <= 2000 :
+        return 20
+    elif year > 2000 and year <= 2100 :
+        return 21
